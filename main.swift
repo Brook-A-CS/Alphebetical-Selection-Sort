bdc7698688050bc3b7732a69f49dc3bde5cd709f
Brook Asnake <brook.asnake@student.allenisd.org>
2022-10-06 14:31:09 -0500

    Selection Sort

---
+++ main.swift
@@ -0,0 +1,43 @@
+func isCapitalized(word: String) {
+    
+}
+
+var arrayOfWords = [String]()
+
+func read(_ array: inout [String]) {    
+    var line : String?
+    
+    repeat {
+        line = readLine()
+        if line != nil {
+            array.append(line!)
+//            print(line!)
+        }
+    } while line != nil
+}
+
+func printArray(array: [String]) {
+    for element in array {
+        print(element)
+    }
+}
+
+func selectionSorter(_ unsorted: inout [String] ) {
+    for index in 0 ..< unsorted.count - 1 {
+        var tempSmallest = index
+        for i in index ..< unsorted.count {
+   
+            if unsorted[i] < unsorted[tempSmallest] {
+                tempSmallest = i                
+            }
+            if i == unsorted.count - 1 {
+                unsorted.swapAt(tempSmallest, index)
+//                print("Pass: \(index+1), Swaps: 1/\(index+1), Array: \(unsorted)")               
+            }
+        }
+    }
+}
+
+read(&arrayOfWords)
+selectionSorter(&arrayOfWords)
+printArray(array: arrayOfWords)
